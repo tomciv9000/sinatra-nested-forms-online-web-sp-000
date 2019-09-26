a18d0fbb6e40a446a6caa511e545985de63af4c6
Tom White <eugene.white@gmail.com>
2019-09-26 18:30:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,14 @@ module FormsLab
     end
     
     post '/pirates' do
-      erb :
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
+
+      erb :'pirates/show'
     end
     
   end
