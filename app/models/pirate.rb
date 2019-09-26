525b7d4a545a9a9e4b68b35a7674cea84f389288
Tom White <eugene.white@gmail.com>
2019-09-26 17:49:04 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -3,10 +3,10 @@ class Pirate
   
   @@all = []
   
-  def initialize(name, weight, height)
-    @name = name
-    @weight = weight
-    @height = height
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
     @@all << self
   end
   
